fa672fb4de24457e9d8efae388de80851bb067b1
Vitor Martins <viitormartins@hotmail.com>
2017-11-24 00:32:14 -0200

    add components de home e tarefa

--- agenda-app/src/pages/home/home.ts
+++ agenda-app/src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+
+import { TaskPage } from '../task/task';
+import { NewTaskPage } from '../new-task/new-task';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public taskList;
 
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.taskList = [
+      {
+        dataInicio: '10/10/2010 0:00',
+        dataFim: '10/10/2010 2:00',
+        descricao: 'Ir buscar meu carro na oficina',
+        nome: 'Mecânico'
+      },
+      {
+        dataInicio: '11/11/2011 8:00',
+        dataFim: '11/11/2011 9:00',
+        descricao: 'Dar banho no cachorro',
+        nome: 'Banho cachorro'
+      }
+    ]
+  }
+
+  selectTask(task) {
+    this.navCtrl.push(TaskPage, { taskSelected: task });
+  }
+
+  addTask(){
+    this.navCtrl.push(NewTaskPage);
   }
 
 }
---
+++ agenda-app/src/pages/task/task.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+import { NavParams, NavController } from "ionic-angular";
+
+@Component({
+    templateUrl: 'task.html'
+})
+export class TaskPage {
+
+    public task;
+
+    constructor(public navParams: NavParams, public navCtrl: NavController){
+        this.task = this.navParams.get('taskSelected');
+    }
+}
